7aa471c998562d18a0ba17de5a3520293a45641a
铁蛋 <1132125385@qq.com>
2024-06-30 17:06:26 +0800

    添加 备注

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -6,15 +6,14 @@
     </main>
 </template>
 <script setup lang="ts">
-import Map from './Map.vue';  // 地图
-import Player from './Player.vue';  // 玩家  
-import Cargos from './Cargos.vue';
+import Map from './Map.vue';  // 地图组件
+import Player from './Player.vue';  // 玩家组件
+import Cargos from './Cargos.vue';   // 箱子组件
 </script>
 
 <style scoped>
 main {
     position: relative;
-    
 }
 
 </style>
