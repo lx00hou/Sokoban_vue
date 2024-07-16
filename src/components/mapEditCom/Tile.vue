875d5b35bce978ad0c83098b4c9560c549fceacb
铁蛋 <1132125385@qq.com>
2024-07-16 21:15:07 +0800

    完善 地图编辑器 (创建 Tile 组件)

---
+++ src/components/mapEditCom/Tile.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img :src="imgSrc" :alt="title" @click="handleClick(title)">
+    </div>
+</template>
+<script setup lang="ts">
+interface Prop {
+    title:string,
+    imgSrc:string
+}
+defineProps<Prop>();
+const emit = defineEmits(['selTail']);
+
+const handleClick = (title:string) => {
+    emit('selTail',title)
+}
+</script>
+
+<style scoped>
+div {
+    width: 80px;
+    height: 80px;
+    margin-right: 20px;
+}
+img {
+    width: 100%;
+}
+</style>
+
--- src/view/MapEdit.vue
+++ src/view/MapEdit.vue
@@ -1,17 +1,34 @@
 <template>
     <main>
         <!-- 地图编辑区域 -->
-        <div class="mapEditArea" v-for="row in 10">
-            <div v-for="j in 10">
+        <div class="mapEditArea" v-for="row in rows">
+            <div v-for="j in cols">
                 <MapBlock />
             </div>
         </div>
+        <!-- 选择地图组件 -->
+        <section class="mapComponent">
+            <Tile title="地板" :imgSrc="floor" @selTail="title => curSelTail = title" />
+            <Tile  title="墙体" :imgSrc="wall"  @selTail="title => curSelTail = title" />
+        </section>
+
+        <section class="sel" v-if="curSelTail" >
+            您当前选中了: {{ curSelTail }}
+        </section>
     </main>
 </template>
 <script setup lang="ts">
-import MapBlock from '../components/mapEditCom/mapBlock.vue'
+import floor from '../assets/floor.png';
+import wall from '../assets/wall.png';
+import MapBlock from '../components/mapEditCom/mapBlock.vue';
+import Tile from '../components/mapEditCom/Tile.vue'
+import { ref } from 'vue';
 
-
+// 设置行列数量
+const rows = ref(10);
+const cols = ref(10);
+// 当前选中的地图组件
+const curSelTail = ref("");
 </script>
 
 <style scoped>
@@ -21,6 +38,12 @@ main {
 .mapEditArea {
     display: flex;
 }
-
+.mapComponent {
+    display: flex;
+    margin-top: 30px;
+}
+.sel {
+    margin-top: 20px;
+}
 </style>
 
